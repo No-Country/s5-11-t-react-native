905af4697c50b85f6caf06289f1796ce8615e2f6
AlexanderC <alex88_upt@hotmail.com>
2022-11-17 14:36:13 -0500

    ItemDetailContainer

--- client/src/components/FoodMenu/ItemDetailContainer.js/ItemDetailContainer.js
+++ client/src/components/FoodMenu/ItemDetailContainer.js/ItemDetailContainer.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { View, Text, SafeAreaView} from 'react-native';
+import { SafeAreaView, Image, TouchableOpacity, View, Text} from 'react-native';
+import ItemDetail from './ItemDetail/ItemDetail';
 import { styles } from './stylesItemDetailContainer';
 
 const ItemDetailContainer = () => {
 
     return (
       <SafeAreaView style={styles.container}>
-        
-     
-    </SafeAreaView>
+        <TouchableOpacity style={styles.cartContainer}>
+            <Image style ={styles.cart} source={require('../../../../assets/Icons/Cart.png')}/>
+          </TouchableOpacity>
+        <ItemDetail/>
+        <View style={styles.footerContainer}>
+          <TouchableOpacity style={styles.optionContainer}>
+            <Image  source={require('../../../../assets/Icons/Basket.png')}/>
+            <Text style={styles.footerText}>Volver</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.optionContainer}>
+            <Image  source={require('../../../../assets/Icons/Cart.png')}/>
+            <Text style={styles.footerText}>Añadir</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
     );
 }
 
